6f2be3277d6a45a0f81def078ceec70bf1da1835
vaishnavipriyajs-blip <vaishnavipriyajs@student.tce.edu>
2026-06-21 14:31:02 +0530

    Update out.py

--- gradcam_explainability/src/out.py
+++ gradcam_explainability/src/out.py
@@ -1,1 +1,15 @@
 
+video_path = list(video.keys())[0]
+
+path, cam = generate_gradcam(video_path)
+
+print("\n🧠 EXPLANATION:\n")
+exp = explain_heatmap(cam)
+
+for line in exp:
+    print(line)
+
+from IPython.display import Image, display
+
+print("\n🖼️ OUTPUT IMAGE:")
+display(Image(path))
